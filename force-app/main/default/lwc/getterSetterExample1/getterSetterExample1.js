1ef9c187e97ac3d823e7fa592035856139b348aa
sfdcstation <sfdcstation@gmail.com>
2021-06-21 13:31:55 +0530

    2nd commit for getter and setter

--- force-app/main/default/lwc/getterSetterExample1/getterSetterExample1.js
+++ force-app/main/default/lwc/getterSetterExample1/getterSetterExample1.js
@@ -1,3 +1,35 @@
 import { LightningElement } from 'lwc';
 
-export default class GetterSetterExample1 extends LightningElement {}
+export default class GetterSetterExample1 extends LightningElement {
+
+    numbertoconvert;
+    convertedValue;
+    selectedOption;
+
+    get options() {
+        return [
+            { label: '--None--', value: '' },
+            { label: 'Second', value: '1000' },
+            { label: 'Minute', value: '60000' },
+            { label: 'Hour', value: '3.6e+6' },
+            { label: 'Day', value: '8.64e+7' }
+        ];
+    }
+
+    set convert(val) {
+        this.convertedValue = val;
+    }
+
+    handleChange(event) {
+        this.numbertoconvert = event.detail.value;
+    }
+
+    handleSelect(event) {
+        this.selectedOption = event.detail.value;
+    }
+
+    handleClick() {
+        this.convert = (this.selectedOption && this.numbertoconvert ? this.numbertoconvert * this.selectedOption : '');
+    }
+
+}
