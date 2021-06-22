666888e129d13a91c5a1ca37b5bcf5870b562007
sfdcstation <sfdcstation@gmail.com>
2021-06-22 18:44:10 +0530

    imperative template

---
+++ Imperative/force-app/main/default/lwc/imperativeExample1/imperativeExample1.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+import findContacts from '@salesforce/apex/ContactController.findContacts';
+
+export default class ImperativeExample1 extends LightningElement
+ {
+    searchKey = '';
+    contacts;
+    error;
+
+    handleKeyChange(event) {
+        this.searchKey = event.target.value;
+    }
+
+    handleSearch() {
+        findContacts({ searchKey: this.searchKey })
+            .then((result) => {
+                this.contacts = result;
+                this.error = undefined;
+            })
+            .catch((error) => {
+                this.error = error;
+                this.contacts = undefined;
+            });
+    }
+
+}
