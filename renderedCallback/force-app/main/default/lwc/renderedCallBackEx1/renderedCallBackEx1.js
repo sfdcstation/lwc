8f250b3ab6f94a28371a1cd5b073cad474c1bccf
sfdcstation <sfdcstation@gmail.com>
2021-06-23 17:51:14 +0530

    rendered callback

--- renderedCallback/force-app/main/default/lwc/renderedCallBackEx1/renderedCallBackEx1.js
+++ renderedCallback/force-app/main/default/lwc/renderedCallBackEx1/renderedCallBackEx1.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class RenderedCallBackEx1 extends LightningElement {}
+export default class RenderedCallBackEx1 extends LightningElement 
+{
+
+    properties;
+    renderedCallback() {
+        this.properties = 'set by renderedCallback';
+        console.log('properties ' + this.properties);
+    }
+
+    handleButtonClick() {
+        this.properties = 'set by buttonClick';
+    }
+
+}
